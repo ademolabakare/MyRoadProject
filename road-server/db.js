2f8eecd493f6dfe31ae1a6bf9dc89ddcae07aaf8
ademolabakare <106655720+ademolabakare@users.noreply.github.com>
2024-09-11 01:50:36 -0700

    Update db.js

--- road-server/db.js
+++ road-server/db.js
@@ -4,8 +4,8 @@ const { Pool } = require('pg');
 const pool = new Pool({
   user: 'postgres',
   host: 'localhost',
-  database: 'road_condition_db',
-  password: 'adebabalola',
+  database: 'db',
+  password: 'password',
   port: 5432,  // Default PostgreSQL port
 });
 
